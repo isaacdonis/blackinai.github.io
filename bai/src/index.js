305708a6eef3a8d31f8bbbc4f935b14d8947c803
mirianfsilva <mirianfrsilva@gmail.com>
2021-01-05 00:09:11 -0300

    wip deploy all pages

--- bai/src/index.js
+++ bai/src/index.js
@@ -18,7 +18,7 @@ import Conferences from './pages/BaiWorkshops/AllConferences';
 library.add(fab, fas);
 
 ReactDOM.render(
-    <HashRouter basename='/#/' hashType={'slash'} >
+    <HashRouter basename='/' >
         <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/about" component={About} />
