d9886b742a7f56d0bae6a2ba56f996e392558b5c
mirianfsilva <mirianfrsilva@gmail.com>
2021-01-22 02:59:51 -0300

    update responsive home header

--- bai/src/components/HomeHeader/index.js
+++ bai/src/components/HomeHeader/index.js
@@ -21,10 +21,10 @@ const styles = (theme) => ({
         backgroundPosition: 'center',
     },
     container: {
+        display: 'flex',
+        direction: 'column',
         marginTop: theme.spacing(8),
         marginBottom: theme.spacing(8),
-        display: 'flex',
-        position: 'relative',
     },
     item: {
         display: 'flex',
@@ -80,31 +80,29 @@ function HomeHeader(props) {
 
     return (
         <Container className={classes.container}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} className={classes.container}>
-                        <Grid item xs={12} md={6}>
-                            <Typography align="left" variant="h3" className={classes.title}>
-                                Increasing the presence of black people in the field of artificial intelligence
-                            </Typography>
-                            <Typography align="left" variant="h5">
-                                Black in AI increases the presence and inclusion of Black people in the field of 
-                                AI by creating space for sharing ideas, fostering collaborations, mentorship and advocacy.
-                            </Typography>
-                            <ColorButton className={classes.chip} variant="contained" href="/#/membership">
-                                Become a Member
-                            </ColorButton>
-                            <JoinUsButton className={classes.chip} variant="contained" href="/#/partnership">
-                                Partner with Us
-                            </JoinUsButton>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <CardMedia component="img" height="400" width="350" image={image}
-                                alt="Almost ten black people, smiling, with raised hands. All wearing the black in AI badge.">
-                            </CardMedia>
-                        </Grid>
-                    </Grid>
+            <Grid container spacing={4}>
+                <Grid item xs={12} md={6}>
+                    <Typography align="left" variant="h3" className={classes.title}>
+                        Increasing the presence of black people in the field of artificial intelligence
+                    </Typography>
+                    <Typography align="left" variant="h5">
+                        Black in AI increases the presence and inclusion of Black people in the field of
+                        SAI by creating space for sharing ideas, fostering collaborations, mentorship and advocacy.
+                    </Typography>
+                    <ColorButton className={classes.chip} variant="contained" href="/#/membership">
+                        Become a Member
+                    </ColorButton>
+                    <JoinUsButton className={classes.chip} variant="contained" href="/#/partnership">
+                        Partner with Us
+                    </JoinUsButton>
                 </Grid>
-            </Container>
+                <Grid item xs={12} md={6}>
+                    <CardMedia component="img" height="400" image={image}
+                        alt="Almost ten black people, smiling, with raised hands. All wearing the black in AI badge.">
+                    </CardMedia>
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
 
