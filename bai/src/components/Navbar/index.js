5af8dd7b9600055545400a080b425b7c0c8aef7c
Mirian Silva <mirianfrsilva@ibm.com>
2023-03-10 13:18:29 -0300

    update paypal donate

--- bai/src/components/Navbar/index.js
+++ bai/src/components/Navbar/index.js
@@ -112,17 +112,10 @@ const ColorButton = withStyles((theme) => ({
 
 function Donate() {
     return (
-        <form id='my-donate-form' action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" rel="noopener">
-            <input type="hidden" name="cmd" value="_donations" />
-            <input type="hidden" name="business" value="organizers@blackinai.org" />
-            <input type="hidden" name="lc" value="US" />
-            <input type="hidden" name="item_name" value="Black in AI Fundraiser" />
-            <input type="hidden" name="item_number" value="Thanks for your kind contribution. Funds are used to support Black in AI initiatives." />
-            <input type="hidden" name="no_note" value="0" />
-            <input type="hidden" name="currency_code" value="USD" />
-            <input type="hidden" name="bn" value="PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest" />
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-            <img alt="paypal button for donations" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="3" height="3" />
+        <form action="https://www.paypal.com/donate" method="post" target="_top">
+            <input type="hidden" name="hosted_button_id" value="2LFFVSWVN9ZQ6" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
         </form>
     );
 }
